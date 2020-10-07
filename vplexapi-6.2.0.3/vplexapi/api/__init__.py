2fd48a8027fc0beb01c3283c8989e7f5f75891c8
Anil Degwekar <anil.degwekar@emc.com>
2020-10-07 15:02:34 +0530

    Initial Release - API version 6.2.0.3

---
+++ vplexapi-6.2.0.3/vplexapi/api/__init__.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from vplexapi.api.aggregate_api import AggregateApi
+from vplexapi.api.amp_api import AmpApi
+from vplexapi.api.bulk_api import BulkApi
+from vplexapi.api.certificates_api import CertificatesApi
+from vplexapi.api.cluster_witness_api import ClusterWitnessApi
+from vplexapi.api.clusters_api import ClustersApi
+from vplexapi.api.consistency_group_api import ConsistencyGroupApi
+from vplexapi.api.data_migration_api import DataMigrationApi
+from vplexapi.api.devices_api import DevicesApi
+from vplexapi.api.director_api import DirectorApi
+from vplexapi.api.distributed_storage_api import DistributedStorageApi
+from vplexapi.api.engine_api import EngineApi
+from vplexapi.api.exports_api import ExportsApi
+from vplexapi.api.extent_api import ExtentApi
+from vplexapi.api.hardware_ports_api import HardwarePortsApi
+from vplexapi.api.health_api import HealthApi
+from vplexapi.api.jobs_api import JobsApi
+from vplexapi.api.licenses_api import LicensesApi
+from vplexapi.api.logger_api import LoggerApi
+from vplexapi.api.logging_volume_api import LoggingVolumeApi
+from vplexapi.api.logical_units_api import LogicalUnitsApi
+from vplexapi.api.maps_api import MapsApi
+from vplexapi.api.meta_volume_api import MetaVolumeApi
+from vplexapi.api.monitors_api import MonitorsApi
+from vplexapi.api.permissions_api import PermissionsApi
+from vplexapi.api.recoverpoint_api import RecoverpointApi
+from vplexapi.api.storage_array_api import StorageArrayApi
+from vplexapi.api.storage_group_api import StorageGroupApi
+from vplexapi.api.storage_pool_api import StoragePoolApi
+from vplexapi.api.storage_volume_api import StorageVolumeApi
+from vplexapi.api.system_config_api import SystemConfigApi
+from vplexapi.api.token_api import TokenApi
+from vplexapi.api.version_api import VersionApi
+from vplexapi.api.virtual_volume_api import VirtualVolumeApi
